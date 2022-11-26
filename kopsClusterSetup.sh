b60a7217b90b1b388a5d7f92436670093e657575
pyvivid <94853400+pyvivid@users.noreply.github.com>
2022-11-26 13:40:35 +0530

    Create kopsClusterSetup.sh

---
+++ kopsClusterSetup.sh
@@ -0,0 +1,7 @@
+## Steps to setup Kops cluster
+## Only for auth login step, do it externally
+#!/bin/bash
+curl -Lo kops https://github.com/kubernetes/kops/releases/download/$(curl -s https://api.github.com/repos/kubernetes/kops/releases/latest | grep tag_name | cut -d '"' -f 4)/kops-linux-amd64
+chmod +x ./kops
+sudo mv ./kops /usr/local/bin/
+
